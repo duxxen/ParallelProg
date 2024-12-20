f2052dba7d9fbd00351bda49b0fadceccc1187bc
duxxen <a.romanovu@gmail.com>
2024-12-20 20:50:36 +0300

    deleted old

--- lab2/lab2.h
+++
@@ -1,8 +0,0 @@
-#pragma once
-#include "../common.h"
-
-namespace LR2
-{
-	#include <omp.h>
-	int lab2(int argc, char* argv[]);
-}
--- lab3/lab3.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-#include "../common.h"
-
-namespace LR3
-{
-	#define HAVE_STRUCT_TIMESPEC
-	#include <pthread.h>
-	int lab3(int argc, char* argv[]);
-}
